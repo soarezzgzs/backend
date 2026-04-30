54a476af1d63882e85633f9af437318d1b007ba4
Leonardo Soares <leonardo.soares3345@gmail.com>
2026-04-30 20:58:39 -0300

    connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,7 @@
 import knex from 'knex';
 import config from '../../knexfile.js';
 
-const connection = knex(config.development);
+const environment = process.env.NODE_ENV || 'development';
+const db = knex(config[environment]);
 
-export default connection;
+export default db;
