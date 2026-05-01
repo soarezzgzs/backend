39083de51ff08910fb31f318bcad98ac3a4bfc78
Leonardo Soares <leonardo.soares3345@gmail.com>
2026-04-30 21:08:25 -0300

    Rollback

--- src/server.js
+++ src/server.js
@@ -6,9 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 app.use(routes);
-app.use('/', (res, res) => {
-    res.send('API de Vinis rodando!')
-})
+
 app.listen(3333, () => {
   console.log('Servidor rodando na 3333');
 });
